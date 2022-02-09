06ca20b30270585be7f69d5d01574ad29a6f1fbb
wlisuha <wlisuha@gmail.com>
2022-02-09 11:31:48 +0200

    settings layout

--- app/src/main/java/com/wlisuha/applauncher/ui/custom/NotificationView.kt
+++ app/src/main/java/com/wlisuha/applauncher/ui/custom/NotificationView.kt
@@ -1,4 +1,28 @@
 package com.wlisuha.applauncher.ui.custom
 
-class NotificationView {
+import android.content.Context
+import android.graphics.Color
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.wlisuha.applauncher.R
+import com.wlisuha.applauncher.databinding.NotificationViewBinding
+
+class NotificationView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : ConstraintLayout(context, attrs, defStyle) {
+
+    val binding: NotificationViewBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context),
+        R.layout.notification_view,
+        this,
+        true
+    )
+
+    init {
+        setBackgroundColor(Color.RED)
+    }
 }
