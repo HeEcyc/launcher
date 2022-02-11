e31172e0f79f4da28e5bf8500e8c583c0369db07
wlisuha <wlisuha@gmail.com>
2022-02-11 15:06:14 +0200

    fix progress bars

--- app/src/main/java/com/wlisuha/applauncher/ui/custom/ShutterView.kt
+++ app/src/main/java/com/wlisuha/applauncher/ui/custom/ShutterView.kt
@@ -19,7 +19,6 @@ import android.os.Looper
 import android.os.PowerManager
 import android.provider.Settings
 import android.util.AttributeSet
-import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
