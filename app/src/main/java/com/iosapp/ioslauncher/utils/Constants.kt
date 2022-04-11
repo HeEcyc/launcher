79ea3ca0a41d83646c1188976378876c3d9dab0e
Nikita <niklog21@gmail.com>
2022-04-11 16:17:02 +0300

    Changed IronSource api key

--- app/src/main/java/com/iosapp/ioslauncher/utils/Constants.kt
+++ app/src/main/java/com/iosapp/ioslauncher/utils/Constants.kt
@@ -5,7 +5,7 @@ const val ENABLED_ANIMATION_DURATION = 200L
 const val MOVING_PAGE_DELAY = 500L
 const val REQUEST_DELAY = 1000L
 
-const val IRON_SOURCE_APP_KEY = ""
+const val IRON_SOURCE_APP_KEY = "145850121"
 const val EXTRAS_HIDING = "extras_hiding"
 
 const val YEAR = 2022; const val MONTH = 9; const val DATE = 22
