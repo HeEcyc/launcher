a007c36c12eee659c7932f048ea4340d5383a14f
Nikita <niklog21@gmail.com>
2022-05-25 15:15:56 +0300

    Disabled hiding, disabled crashlytics and removed ironSource app key

--- app/src/main/java/com/iosapp/ioslauncher/ui/app/AppActivity.kt
+++ app/src/main/java/com/iosapp/ioslauncher/ui/app/AppActivity.kt
@@ -4,9 +4,7 @@ import android.Manifest
 import android.app.role.RoleManager
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Build
-import android.provider.Settings
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.constraintlayout.motion.widget.MotionLayout
@@ -15,7 +13,6 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.viewpager.widget.ViewPager
-import com.iosapp.ioslauncher.LauncherApplication
 import com.iosapp.ioslauncher.R
 import com.iosapp.ioslauncher.base.BaseActivity
 import com.iosapp.ioslauncher.data.Prefs
@@ -24,14 +21,8 @@ import com.iosapp.ioslauncher.ui.custom.ShutterView
 import com.iosapp.ioslauncher.ui.dialogs.DialogNotificationsPermissions
 import com.iosapp.ioslauncher.ui.dialogs.DialogPermission
 import com.iosapp.ioslauncher.ui.dialogs.DialogTutorial
-import com.iosapp.ioslauncher.utils.EXTRAS_HIDING
 import com.iosapp.ioslauncher.utils.IRON_SOURCE_APP_KEY
-import com.iosapp.ioslauncher.utils.hiding.AlarmBroadcast
-import com.iosapp.ioslauncher.utils.hiding.AppHidingUtil
-import com.iosapp.ioslauncher.utils.hiding.HidingBroadcast
 import com.ironsource.mediationsdk.IronSource
-import java.util.*
-
 
 class AppActivity : BaseActivity<AppViewModel, AppActivityBinding>(R.layout.app_activity),
     ShutterView.PermissionHelper {
@@ -52,29 +43,9 @@ class AppActivity : BaseActivity<AppViewModel, AppActivityBinding>(R.layout.app_
     }
     override val viewModel: AppViewModel by viewModels()
 
-    override fun onNewIntent(intent: Intent?) {
-        if (intent?.getStringExtra(EXTRAS_HIDING) == EXTRAS_HIDING) {
-            if (!Settings.canDrawOverlays(this)) {
-                startActivity(
-                    Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION)
-                        .setData(Uri.fromParts("package", LauncherApplication.instance.packageName, null))
-                )
-            }
-        }
-        super.onNewIntent(intent)
-    }
-
-    private fun notSupportedBackgroundDevice() = Build.MANUFACTURER.lowercase(Locale.ENGLISH) in listOf(
-        "xiaomi", "oppo", "vivo", "letv", "honor", "oneplus"
-    )
-
     override fun onResume() {
         super.onResume()
         IronSource.onResume(this)
-        if (Settings.canDrawOverlays(this) && notSupportedBackgroundDevice())
-            AppHidingUtil.hideApp(this, "Launcher2", "Launcher")
-        else
-            HidingBroadcast.startAlarm(this)
     }
 
     override fun onPause() {
@@ -86,7 +57,6 @@ class AppActivity : BaseActivity<AppViewModel, AppActivityBinding>(R.layout.app_
     override fun setupUI() {
         IronSource.setMetaData("is_child_directed","false")
         IronSource.init(this, IRON_SOURCE_APP_KEY)
-        AlarmBroadcast.startAlarm(this)
         binding.mainPages.adapter = MainVPAdapter(viewModel, this, binding.mainPages)
         binding.mainPages.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
             override fun onPageScrolled(
